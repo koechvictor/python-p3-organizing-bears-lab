a880e09b4a261242e8fd53d8a84ba5fe8468387b
koech victor <106955701+koechvictor@users.noreply.github.com>
2024-09-08 17:48:13 +0300

    inserting data sql file update

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'brown', 'friendly', 1),
+('Rowdy', 5, 'M', 'brown', 'rowdy', 1),
+('Tabitha', 7, 'F', 'black', 'calm', 1),
+('Sergeant Brown', 12, 'M', 'brown', 'serious', 1),
+('Melissa', 8, 'F', 'white', 'gentle', 1),
+('Grinch', 6, 'M', 'green', 'grumpy', 1),
+('Wendy', 4, 'F', 'yellow', 'happy', 1),
+(NULL, 3, 'F', 'brown', 'unknown', 1);
